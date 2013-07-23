921aad4824ebc9a86f30aaac00c8e9d59c45a02e
Joey Meyer <jmeyer41@gmail.com>
2013-07-23 11:21:17 -0400

    fix indentation in tumblr_to_dayone.rb

--- lib/tumblr_to_dayone.rb
+++ lib/tumblr_to_dayone.rb
@@ -3,15 +3,15 @@ require 'tumblr_to_dayone/tumblr'
 require 'tumblr_to_dayone/dayone'
 
 module TumblrToDayone
-  
+
 end
 
 module Tumblr
-	class Post
+  class Post
 
-		def add_to_dayone(starred = false, dayone_journal = nil)
-  		puts "Added!"
-		end
+    def add_to_dayone(starred = false, dayone_journal = nil)
+      puts "Added!"
+    end
 
-	end
+  end
 end
